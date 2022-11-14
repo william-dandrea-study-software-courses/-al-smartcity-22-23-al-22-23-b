bf95e3e7637e58884fc0492c24d30ec92b6583e5
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-11-14 20:04:06 +0100

    scenario

--- charge.py
+++ charge.py
@@ -3,17 +3,17 @@ import requests
 
 def launchCar():
 
-    for i in range(0, 100):
+    for i in range(0, 1000):
         requests.post('http://localhost:6808/start-car', {"license_plate": str(i)})
 
 
 def changeInterval():
     for i in range(0, 100):
-        requests.post('http://localhost:6808/edit-request-interval', {"license_plate": str(i), "interval": 0.01})
+        requests.post('http://localhost:6808/edit-request-interval', {"license_plate": str(i), "interval": 0.1})
 
 def stopCar():
 
-    for i in range(0, 100):
-        requests.post('http://localhost:6808/stop-car', {"license_plate": str(i)})
+    for i in range(0, 1000):
+        print(requests.post('http://localhost:6808/stop-car', {"license_plate": f"{i}"}))
 
 stopCar()
