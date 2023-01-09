cba94c49d309349955ddf9c459995c7856f9b82b
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2023-01-09 21:10:40 +0100

    Implémentation du micro-service qui génére des itinéraires via OSM (ATTENTION, il faut rebuild l'image via le start-dev)

--- project/run_manager/kafka-exporter/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while ! bin/kafka_exporter; do
-    echo "Waiting for the Kafka cluster to come up..."
-    sleep 10
-done
--- project/start-dev.sh
+++ project/start-dev.sh
@@ -5,6 +5,13 @@ read value
 
 if [ $value -eq 1 ]
 then
+
+  curl -o ./run_manager/osmr/ile-de-france.osm.pbf https://download.geofabrik.de/europe/france/ile-de-france-latest.osm.pbf
+
+  docker run -v $(pwd)/run_manager/osmr:/data osrm/osrm-backend osrm-extract -p /opt/car.lua /data/ile-de-france.osm.pbf
+  docker run -t -v $(pwd)/run_manager/osmr:/data  osrm/osrm-backend osrm-partition /data/ile-de-france.osm
+  docker run -t -v $(pwd)/run_manager/osmr:/data  osrm/osrm-backend osrm-customize /data/ile-de-france.osm
+
   docker-compose --project-name smartcity-dev --file ./docker-compose-dev.yml build --parallel
 fi
 
