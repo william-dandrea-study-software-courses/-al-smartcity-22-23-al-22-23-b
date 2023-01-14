2420ce78286f63db7310ccec9545d91a1681ba79
Nicolas-Fern <nicolas.fernandez@etu.unice.fr>
2023-01-14 16:54:02 +0100

    Prerequirement

--- project/main/user-client/src/pages/NavPage.js
+++ project/main/user-client/src/pages/NavPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import { StartStop } from "../components";
 import { TestDebug } from "../components/TestDebug";
 
+import { MapContainer, TileLayer} from "react-leaflet";
+
+const maptiler = {
+    url: "https://api.maptiler.com/maps/basic-2154/?key=KcNMZD3S4dFh93cyStfs#9.9/48.86758/2.32928",
+    attribution:
+    '&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+}
+
 const NavPage = () => {
-  console.log("in home page");
+    const [center, setCenter] = useState({lat: 48.234578, lon:2.76932})
+    const ZOOM_LVL=9
+    const ref = useRef()
+  console.log("inb   home page");
 
   return (
     <>
       <StartStop />
-      {/* <TestDebug /> */}
+      <div>
+          <MapContainer
+          center={center}
+          zoom={ZOOM_LVL}
+          ref={ref}
+          >
+            <TileLayer url={maptiler.url} attribution = {maptiler.attribution}/>
+          </MapContainer>
+      </div>
     </>
   );
 };
